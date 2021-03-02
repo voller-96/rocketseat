08020470c0e0edaf3e837d7279ed07f908880c5f
voller <voller.silas@gmail.com>
2021-03-02 10:44:25 -0300

    rocketseat

---
+++ Tipos de dados/script.js
@@ -0,0 +1,23 @@
+// String
+console.log("voller é inteligente")
+
+//number
+console.log(33)
+console.log(45/23)
+
+// boolean
+console.log(true)
+console.log(false)
+
+//objects
+console.log({
+  name:"voller",
+  idade:24,
+  andar: function(){
+    console.log("andar")
+  }
+})
+
+//array
+console.log(["voller",24])
+console.log(["ovos","cenoura","pepino"])
---
+++ primeiros passos/script.js
@@ -0,0 +1,5 @@
+console.log("Bem vindo ao starter!")
+console.log("Sou o voller.")
+// aqui eu tenho um comentário
+/* 
+Comentário de bloco */
